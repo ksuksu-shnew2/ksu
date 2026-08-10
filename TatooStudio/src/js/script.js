2a3a20196e8bde6e8370cacb741f94659295ab8e
KSU <sh_ksenya@mail.com>
2026-08-11 01:38:28 +0300

    tatto

--- TatooStudio/src/js/script.js
+++ TatooStudio/src/js/script.js
@@ -1,1 +1,16 @@
 import "../sass/style.scss";
+
+const cards = document.querySelectorAll(".about__animation");
+
+cards.forEach((card) => {
+    card.addEventListener("click", () => {
+
+        cards.forEach((item) => {
+            if (item !== card) {
+                item.classList.remove("is-active");
+            }
+        });
+
+        card.classList.toggle("is-active");
+    });
+});
